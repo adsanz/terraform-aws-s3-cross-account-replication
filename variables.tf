86144948fff0c8f5ab1bf9fde2049f744aa4f933
Adrian Sanz <asanz@mrmilu.com>
2022-03-28 16:42:22 +0200

    Refactor: fix dest name

--- variables.tf
+++ variables.tf
@@ -9,15 +9,9 @@ variable "replicate_prefix" {
   default     = ""
 }
 
-variable "dest_bucket_name" {
+variable "bucket_dest_name" {
   type        = string
   description = "Name for dest s3 bucket"
-}
-
-variable "create_dest_bucket" {
-  type        = string
-  description = "Boolean for whether this module should create the destination bucket"
-  default     = "true"
 }
 
 variable "replication_name" {
