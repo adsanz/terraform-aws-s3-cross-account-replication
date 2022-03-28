8e5692243a7b0a3fff4ab71a821058cdcaf73304
Adrian Sanz <asanz@mrmilu.com>
2022-03-28 16:21:03 +0200

    Refactor: removed empty providers

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # MAIN
 
-provider "aws" {
-  alias = "source"
-}
-
-provider "aws" {
-  alias = "dest"
-}
+#provider "aws" {
+#  alias = "source"
+#}
+#
+#provider "aws" {
+#  alias = "dest"
+#}
 
 data "aws_caller_identity" "source" {
   provider = aws.source
